c78270ba73ab340c2ac2d46c251e82a67e7f1089
xpnye <xpnye@DESKTOP-59BEK0T>
2021-03-15 23:10:47 +0100

    email

---
+++ emailapp/src/emailappfeladat/app.java
@@ -0,0 +1,75 @@
+package emailappfeladat;
+
+import java.util.Scanner;
+
+public class app {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity = 500;
+	private String alternateEmail;
+	private String email;
+	
+	
+	
+	public app(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+		
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
+		
+		this.password = randomPassword(10);
+		System.out.println("Your password is: " + this.password);
+		
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + ".aeycompany.com";
+		
+		System.out.println("Your email is: " + email);
+		
+		
+	}
+	
+	private String setDepartment() {
+		System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter code: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {return "sales";}
+		else if (depChoice == 2) {return "dev";}
+		else if (depChoice == 3) {return "acct";}
+		else {return "";}
+		
+		
+	}
+	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$%?";
+		char[] password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
+	
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
+	
+	public void setAlternateEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	}
+	
+	public void changePassword(String password) {
+		this.password = password;
+	}
+	
+	public int getMailboxCapacity() {return mailboxCapacity;}
+	public String getAlternateEmail() {return alternateEmail;}
+	public String getPassword() {return password;}	
+	
+	public String showinfo() {
+		return "Display Name: " + firstName + " " + lastName + "Email: " + email + "Capacity: " + "mailboxCapacity";
+	}
+}
---
+++ emailapp/src/emailappfeladat/email.java
@@ -0,0 +1,12 @@
+package emailappfeladat;
+
+public class email {
+
+	public static void main(String[] args) {
+		app em1 = new app("John","Smith");
+		
+		System.out.println(em1.showinfo());
+
+	}
+
+}
